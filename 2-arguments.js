fca6584d2bda60cbca6202d553e24a11600cc9f3
nasir-beep <nash144@live.com>
2025-11-10 19:44:35 +0200

    Add 2-arguments script

---
+++ 2-arguments.js
@@ -0,0 +1,1 @@
+const args = process.argv.slice(2);  if (args.length === 0) { console.log("No argument"); } else if (args.length === 1) { console.log("Argument founf"); } else { console.log("Arguments founf");}
