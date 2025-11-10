72ce808276d1105bfe7055bc166b94f2c3c5a21a
nasir-beep <nash144@live.com>
2025-11-10 20:56:26 +0200

    Add 5-to_integer script

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,1 +1,1 @@
-const arg = process.argv[2]; const num = parseInt(arg, 10);  if (!isNaN(num)) { console.log("Mynumber: " + num);} else { console.log("Not a number");}
+const[arg] = process.argv.slice(2);   const number = parseInt(arg);  if (isNaN(number)) { console.log("Not a number");} else { console.log("My nummber: ${number}");}
